59c410c8fee38073d5480b7ca98ff344ecf4046a
Simone Locci <simone.locci@cloudesire.com>
2016-01-30 16:58:30 +0100

    Fix IP in property DB_HOSTNAME

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -21,10 +21,10 @@ if [ -z "$DB_NAME" ]; then
 		exit 1
 fi
 
-sed -i "s/\$defaultParameters\['db_hostname'\]/"${DB_HOSTNAME}"/" vtigercrm/modules/Install/views/Index.php
-sed -i "s/\$defaultParameters\['db_username'\]/"${DB_USERNAME}"/" vtigercrm/modules/Install/views/Index.php
-sed -i "s/\$defaultParameters\['db_password'\]/"${DB_PASSWORD}"/" vtigercrm/modules/Install/views/Index.php
-sed -i "s/\$defaultParameters\['db_name'\]/"${DB_NAME}"/" vtigercrm/modules/Install/views/Index.php
+sed -i "s/\$defaultParameters\['db_hostname'\]/'"${DB_HOSTNAME}"'/" vtigercrm/modules/Install/views/Index.php
+sed -i "s/\$defaultParameters\['db_username'\]/'"${DB_USERNAME}"'/" vtigercrm/modules/Install/views/Index.php
+sed -i "s/\$defaultParameters\['db_password'\]/'"${DB_PASSWORD}"'/" vtigercrm/modules/Install/views/Index.php
+sed -i "s/\$defaultParameters\['db_name'\]/'"${DB_NAME}"'/" vtigercrm/modules/Install/views/Index.php
 
 exec "$@"
 
